7b860b0fc6a9f25bde1433ed54c6c6e63934b5d5
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2011-10-18 06:27:20 +0200

    Initial commit

---
+++ Modulefile
@@ -0,0 +1,9 @@
+name 'dhoppe-rsnapshot'
+version '0.0.1'
+
+author 'Dennis Hoppe'
+license 'GPLv2'
+project_page 'http://github.com/dhoppe/puppet-rsnapshot'
+source 'git://github.com/dhoppe/puppet-rsnapshot'
+summary 'Rsnapshot Module'
+description 'Rsnapshot Module for Debian, Ubuntu'
---
+++ manifests/init.pp
@@ -0,0 +1,57 @@
+class rsnapshot {
+	define rsnapshot::host($host = false) {
+		$t_hosts = $host ? {
+			false   => $hostname,
+			default => $host,
+		}
+
+		file { "$name":
+			owner   => root,
+			group   => root,
+			mode    => 0644,
+			alias   => "rsnapshot.conf",
+			content => template("rsnapshot/$lsbdistcodename/etc/rsnapshot.conf.erb"),
+			require => Package["rsnapshot"],
+		}
+	}
+
+	file { "/etc/cron.d/rsnapshot":
+		owner   => root,
+		group   => root,
+		mode    => 0644,
+		source  => "puppet:///modules/rsnapshot/common/etc/cron.d/rsnapshot",
+		require => [
+			File["rsnapshot.conf"],
+			Package["rsnapshot"]
+		],
+	}
+
+	rsnapshot::host { "/etc/rsnapshot.conf":
+		host => [
+			"atlas",
+			"epimetheus",
+			"gaia",
+			"hyperion",
+			"iapetos",
+			"koios",
+			"kreios",
+			"kronos",
+			"menoitios",
+			"mnemosyne",	
+			"okeanos",
+			"phoibe",
+			"prometheus",
+			"rhea",
+			"tethys",
+			"theia",
+			"themis",
+			"titan"
+		],
+	}
+
+	package { "rsnapshot":
+		ensure => present,
+	}
+}
+
+# vim: tabstop=3
