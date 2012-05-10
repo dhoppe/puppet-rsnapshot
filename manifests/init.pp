3df51d30b22f6243838aaa0cf1e16372c51c3ce6
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-05-10 23:47:13 +0200

    Fix several issues detected by puppet-lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,40 +1,22 @@
 class rsnapshot {
-	validate_array(hiera('host'))
+  validate_array(hiera('host'))
 
-	define rsnapshot::host($host = false) {
-		$t_hosts = $host ? {
-			false   => $::hostname,
-			default => $host,
-		}
+  file { '/etc/cron.d/rsnapshot':
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    source  => 'puppet:///modules/rsnapshot/common/etc/cron.d/rsnapshot',
+    require => [
+      File['rsnapshot.conf'],
+      Package['rsnapshot']
+    ],
+  }
 
-		file { "$name":
-			owner   => root,
-			group   => root,
-			mode    => '0644',
-			alias   => 'rsnapshot.conf',
-			content => template("rsnapshot/${::lsbdistcodename}/etc/rsnapshot.conf.erb"),
-			require => Package['rsnapshot'],
-		}
-	}
+  rsnapshot::host { '/etc/rsnapshot.conf':
+    host => hiera('host'),
+  }
 
-	file { '/etc/cron.d/rsnapshot':
-		owner   => root,
-		group   => root,
-		mode    => '0644',
-		source  => 'puppet:///modules/rsnapshot/common/etc/cron.d/rsnapshot',
-		require => [
-			File['rsnapshot.conf'],
-			Package['rsnapshot']
-		],
-	}
-
-	rsnapshot::host { '/etc/rsnapshot.conf':
-		host => hiera('host'),
-	}
-
-	package { 'rsnapshot':
-		ensure => present,
-	}
+  package { 'rsnapshot':
+    ensure => present,
+  }
 }
-
-# vim: tabstop=3
